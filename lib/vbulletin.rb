8628f96705ea6d40958a62466276d90282c28627
Igor Rzegocki <ajgon.spam@gmail.com>
2011-11-02 13:11:10 +0100

    Clean VBulletin database integration (move it to models)

--- lib/vbulletin.rb
+++ lib/vbulletin.rb
@@ -2,6 +2,9 @@ $LOAD_PATH << File.dirname(__FILE__) unless $LOAD_PATH.include?(File.dirname(__F
 
 require 'vbulletin/core_ext'
 require 'vbulletin/models/v_bulletin.rb'
+require 'vbulletin/models/user.rb'
+require 'vbulletin/models/userfield.rb'
+require 'vbulletin/models/usertextfield.rb'
 
 module Vbulletin
 end
