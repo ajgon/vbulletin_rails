7d2600bb2d82bc36c90088a5775dfd06ca1f49bc
Igor Rzegocki <ajgon.spam@gmail.com>
2011-11-16 14:55:28 +0100

    Fixed setting of AR ignore tables

--- lib/vbulletin/core_ext.rb
+++ lib/vbulletin/core_ext.rb
@@ -10,7 +10,7 @@ module ActiveRecord
       #TODO make it parametable
       VBulletin::User.register(:email => self.email, :password => self.password, :username => (self.respond_to?(:username) ? self.username : nil))
     end
-    
+
     def update_vbulletin
       vb_user = VBulletin::User.find_by_email(self.email)
       vb_user.password = self.password
@@ -35,7 +35,7 @@ module ActionController
       session[:vbulletin_userid] = vb_user.userid
       cookies[:bb_lastactivity], cookies[:bb_lastvisit] = vb_session.update_timestamps
       cookies[:bb_sessionhash] = vb_session.sessionhash
-      
+
       set_permanent_vbulletin_session_for vb_user if options[:permanent]
 
       return vb_user
@@ -55,12 +55,12 @@ module ActionController
     def act_as_vbulletin
       if (cookies[:bb_sessionhash] and (vb_session = VBulletin::Session.find_by_sessionhash(cookies[:bb_sessionhash])) and vb_session.userid > 0) or
         (cookies[:bb_userid].to_i > 0 and (vb_user = VBulletin::User.find_by_userid(cookies[:bb_userid])) and vb_user.authenticate_bb_password(cookies[:bb_password]))
-        
+
         if cookies[:bb_userid].to_i > 0 and (vb_user = VBulletin::User.find_by_userid(cookies[:bb_userid])) and vb_user.authenticate_bb_password(cookies[:bb_password])
           vb_session = VBulletin::Session.set(:request => request, :user => vb_user)
           set_permanent_vbulletin_session_for vb_user
         end
-        
+
         session[:vbulletin_userid] = vb_session.userid unless session[:vbulletin_userid]
         cookies[:bb_lastactivity], cookies[:bb_lastvisit] = vb_session.update_timestamps
       else
@@ -74,7 +74,7 @@ module ActionController
       cookies.permanent[:bb_password] = vb_user.bb_password
       session[:vbulletin_permanent] = true
     end
-    
+
     def current_vbulletin_user
       VBulletin::User.find_by_userid(session[:vbulletin_userid])
     end
@@ -88,7 +88,7 @@ module Rails
       def vbulletin
         @vbulletin ||= ActiveSupport::OrderedOptions.new
       end
-      
+
       def vbulletin=(value)
         @vbulletin ||= ActiveSupport::OrderedOptions.new
         @vbulletin.value = value
@@ -120,27 +120,27 @@ module ActiveRecord
 end
 # --
 
-ActiveRecord::SchemaDumper.ignore_tables = ['vb_access', 'vb_action', 'vb_ad', 'vb_adcriteria', 'vb_adminhelp', 'vb_administrator', 'vb_adminlog', 'vb_adminmessage', 'vb_adminutil',
-                                            'vb_album', 'vb_albumupdate', 'vb_announcement', 'vb_announcementread', 'vb_apiclient', 'vb_apilog', 'vb_apipost', 'vb_attachment',
-                                            'vb_attachmentcategory', 'vb_attachmentcategoryuser', 'vb_attachmentpermission', 'vb_attachmenttype', 'vb_attachmentviews', 'vb_autosave',
-                                            'vb_avatar', 'vb_bbcode', 'vb_bbcode_video', 'vb_block', 'vb_blockconfig', 'vb_blocktype', 'vb_bookmarksite', 'vb_cache', 'vb_cacheevent',
-                                            'vb_calendar', 'vb_calendarcustomfield', 'vb_calendarmoderator', 'vb_calendarpermission', 'vb_contentpriority', 'vb_contenttype', 'vb_cpsession',
-                                            'vb_cron', 'vb_cronlog', 'vb_customavatar', 'vb_customprofile', 'vb_customprofilepic', 'vb_datastore', 'vb_dbquery', 'vb_deletionlog',
-                                            'vb_discussion', 'vb_discussionread', 'vb_editlog', 'vb_event', 'vb_externalcache', 'vb_faq', 'vb_filedata', 'vb_forum', 'vb_forumpermission',
-                                            'vb_forumprefixset', 'vb_forumread', 'vb_groupmessage', 'vb_groupmessage_hash', 'vb_groupread', 'vb_holiday', 'vb_humanverify', 'vb_hvanswer',
-                                            'vb_hvquestion', 'vb_icon', 'vb_imagecategory', 'vb_imagecategorypermission', 'vb_indexqueue', 'vb_infraction', 'vb_infractionban',
-                                            'vb_infractiongroup', 'vb_infractionlevel', 'vb_language', 'vb_mailqueue', 'vb_moderation', 'vb_moderator', 'vb_moderatorlog', 'vb_notice',
-                                            'vb_noticecriteria', 'vb_noticedismissed', 'vb_package', 'vb_passwordhistory', 'vb_paymentapi', 'vb_paymentinfo', 'vb_paymenttransaction',
-                                            'vb_phrase', 'vb_phrasetype', 'vb_picturecomment', 'vb_picturecomment_hash', 'vb_picturelegacy', 'vb_plugin', 'vb_pm', 'vb_pmreceipt', 'vb_pmtext',
-                                            'vb_pmthrottle', 'vb_podcast', 'vb_podcastitem', 'vb_poll', 'vb_pollvote', 'vb_post', 'vb_postedithistory', 'vb_posthash', 'vb_postlog',
-                                            'vb_postparsed', 'vb_prefix', 'vb_prefixpermission', 'vb_prefixset', 'vb_product', 'vb_productcode', 'vb_productdependency',
-                                            'vb_profileblockprivacy', 'vb_profilefield', 'vb_profilefieldcategory', 'vb_profilevisitor', 'vb_ranks', 'vb_reminder', 'vb_reputation',
-                                            'vb_reputationlevel', 'vb_route', 'vb_rssfeed', 'vb_rsslog', 'vb_searchcore', 'vb_searchcore_text', 'vb_searchgroup', 'vb_searchgroup_text',
-                                            'vb_searchlog', 'vb_setting', 'vb_settinggroup', 'vb_sigparsed', 'vb_sigpic', 'vb_skimlinks', 'vb_smilie', 'vb_socialgroup',
-                                            'vb_socialgroupcategory', 'vb_socialgroupicon', 'vb_socialgroupmember', 'vb_spamlog', 'vb_stats', 'vb_strikes', 'vb_style', 'vb_stylevar',
-                                            'vb_stylevardfn', 'vb_subscribediscussion', 'vb_subscribeevent', 'vb_subscribeforum', 'vb_subscribegroup', 'vb_subscribethread', 'vb_subscription',
-                                            'vb_subscriptionlog', 'vb_subscriptionpermission', 'vb_tachyforumcounter', 'vb_tachyforumpost', 'vb_tachythreadcounter', 'vb_tachythreadpost',
-                                            'vb_tag', 'vb_tagcontent', 'vb_tagsearch', 'vb_template', 'vb_templatehistory', 'vb_templatemerge', 'vb_thread', 'vb_threadrate', 'vb_threadread',
-                                            'vb_threadredirect', 'vb_threadviews', 'vb_upgradelog', 'vb_useractivation', 'vb_userban', 'vb_userchangelog', 'vb_usercss', 'vb_usercsscache',
-                                            'vb_usergroup', 'vb_usergroupleader', 'vb_usergrouprequest', 'vb_userlist', 'vb_usernote', 'vb_userpromotion', 'vb_usertitle', 'vb_visitormessage',
-                                            'vb_visitormessage_hash']
+ActiveRecord::SchemaDumper.ignore_tables += ['vb_access', 'vb_action', 'vb_ad', 'vb_adcriteria', 'vb_adminhelp', 'vb_administrator', 'vb_adminlog', 'vb_adminmessage', 'vb_adminutil',
+                                             'vb_album', 'vb_albumupdate', 'vb_announcement', 'vb_announcementread', 'vb_apiclient', 'vb_apilog', 'vb_apipost', 'vb_attachment',
+                                             'vb_attachmentcategory', 'vb_attachmentcategoryuser', 'vb_attachmentpermission', 'vb_attachmenttype', 'vb_attachmentviews', 'vb_autosave',
+                                             'vb_avatar', 'vb_bbcode', 'vb_bbcode_video', 'vb_block', 'vb_blockconfig', 'vb_blocktype', 'vb_bookmarksite', 'vb_cache', 'vb_cacheevent',
+                                             'vb_calendar', 'vb_calendarcustomfield', 'vb_calendarmoderator', 'vb_calendarpermission', 'vb_contentpriority', 'vb_contenttype', 'vb_cpsession',
+                                             'vb_cron', 'vb_cronlog', 'vb_customavatar', 'vb_customprofile', 'vb_customprofilepic', 'vb_datastore', 'vb_dbquery', 'vb_deletionlog',
+                                             'vb_discussion', 'vb_discussionread', 'vb_editlog', 'vb_event', 'vb_externalcache', 'vb_faq', 'vb_filedata', 'vb_forum', 'vb_forumpermission',
+                                             'vb_forumprefixset', 'vb_forumread', 'vb_groupmessage', 'vb_groupmessage_hash', 'vb_groupread', 'vb_holiday', 'vb_humanverify', 'vb_hvanswer',
+                                             'vb_hvquestion', 'vb_icon', 'vb_imagecategory', 'vb_imagecategorypermission', 'vb_indexqueue', 'vb_infraction', 'vb_infractionban',
+                                             'vb_infractiongroup', 'vb_infractionlevel', 'vb_language', 'vb_mailqueue', 'vb_moderation', 'vb_moderator', 'vb_moderatorlog', 'vb_notice',
+                                             'vb_noticecriteria', 'vb_noticedismissed', 'vb_package', 'vb_passwordhistory', 'vb_paymentapi', 'vb_paymentinfo', 'vb_paymenttransaction',
+                                             'vb_phrase', 'vb_phrasetype', 'vb_picturecomment', 'vb_picturecomment_hash', 'vb_picturelegacy', 'vb_plugin', 'vb_pm', 'vb_pmreceipt', 'vb_pmtext',
+                                             'vb_pmthrottle', 'vb_podcast', 'vb_podcastitem', 'vb_poll', 'vb_pollvote', 'vb_post', 'vb_postedithistory', 'vb_posthash', 'vb_postlog',
+                                             'vb_postparsed', 'vb_prefix', 'vb_prefixpermission', 'vb_prefixset', 'vb_product', 'vb_productcode', 'vb_productdependency',
+                                             'vb_profileblockprivacy', 'vb_profilefield', 'vb_profilefieldcategory', 'vb_profilevisitor', 'vb_ranks', 'vb_reminder', 'vb_reputation',
+                                             'vb_reputationlevel', 'vb_route', 'vb_rssfeed', 'vb_rsslog', 'vb_searchcore', 'vb_searchcore_text', 'vb_searchgroup', 'vb_searchgroup_text',
+                                             'vb_searchlog', 'vb_setting', 'vb_settinggroup', 'vb_sigparsed', 'vb_sigpic', 'vb_skimlinks', 'vb_smilie', 'vb_socialgroup',
+                                             'vb_socialgroupcategory', 'vb_socialgroupicon', 'vb_socialgroupmember', 'vb_spamlog', 'vb_stats', 'vb_strikes', 'vb_style', 'vb_stylevar',
+                                             'vb_stylevardfn', 'vb_subscribediscussion', 'vb_subscribeevent', 'vb_subscribeforum', 'vb_subscribegroup', 'vb_subscribethread', 'vb_subscription',
+                                             'vb_subscriptionlog', 'vb_subscriptionpermission', 'vb_tachyforumcounter', 'vb_tachyforumpost', 'vb_tachythreadcounter', 'vb_tachythreadpost',
+                                             'vb_tag', 'vb_tagcontent', 'vb_tagsearch', 'vb_template', 'vb_templatehistory', 'vb_templatemerge', 'vb_thread', 'vb_threadrate', 'vb_threadread',
+                                             'vb_threadredirect', 'vb_threadviews', 'vb_upgradelog', 'vb_useractivation', 'vb_userban', 'vb_userchangelog', 'vb_usercss', 'vb_usercsscache',
+                                             'vb_usergroup', 'vb_usergroupleader', 'vb_usergrouprequest', 'vb_userlist', 'vb_usernote', 'vb_userpromotion', 'vb_usertitle', 'vb_visitormessage',
+                                             'vb_visitormessage_hash']
