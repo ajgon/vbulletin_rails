8ea9397a69c2af033e6a5f79eea56b6b79cd4652
Nihad Abbasov <narkoz.2008@gmail.com>
2012-03-31 21:07:06 +0500

    make attributes accessible

--- lib/vbulletin_rails/models/user.rb
+++ lib/vbulletin_rails/models/user.rb
@@ -15,6 +15,7 @@ module VBulletinRails
       set_table_name(PREFIX + 'user')
     end
 
+    attr_accessible :email, :password, :username
     validates_presence_of :email, :password
     validates_uniqueness_of :email
     validates_format_of :email, :with => /^([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})$/i
